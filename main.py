d80ec01014a5d0b37b0900691051f725b37bb34d
Ben Cottier <bcottier2@gmail.com>
2019-04-29 22:02:30 +1000

    Change test environment to continuous and specify DDPG arguments

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ import gym
 
 
 if __name__ == '__main__':
-    env = gym.make('CartPole-v0')
-    env.reset()
-    done = False
-    while not done:
-        env.render()
-        obs, rwd, done, info = env.step(env.action_space.sample()) # take a random action
-    env.close()
-
     # env = gym.make('CartPole-v0')
-    # ddpg(env, 4)
+    # env.reset()
+    # done = False
+    # while not done:
+    #     env.render()
+    #     obs, rwd, done, info = env.step(env.action_space.sample()) # take a random action
+    # env.close()
+
+    env = gym.make('HalfCheetah-v2')
+    ddpg(env, discount=0.99, batch_size=64, polyak=0.001)
