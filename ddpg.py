1f203ccf18c23000f620d638115be0d98de1c156
Ben Cottier <bcottier2@gmail.com>
2019-05-01 13:43:24 +1000

    Add test for polyak operation

--- ddpg.py
+++ ddpg.py
@@ -6,9 +6,11 @@ author: bencottier
 from core import mlp_actor_critic, placeholders
 import gym
 import tensorflow as tf
+import numpy as np
 
 
-def ddpg(env, discount, batch_size, polyak):
+def ddpg(env, discount, batch_size, polyak, actor_critic=mlp_actor_critic,
+        ac_kwargs=dict()):
 
     action_space = env.action_space
     act_dim = action_space.shape[0]
@@ -21,10 +23,10 @@ def ddpg(env, discount, batch_size, polyak):
     d_ph = tf.placeholder(tf.float32, shape=(None,))
 
     with tf.variable_scope('actor-critic'):
-        pi, q, q_pi = mlp_actor_critic(x_ph, a_ph, action_space)
+        pi, q, q_pi = actor_critic(x_ph, a_ph, action_space, **ac_kwargs)
 
     with tf.variable_scope('target'):  # scope helps group the vars for target update
-        pi_targ, q_targ, q_pi_targ = mlp_actor_critic(x2_ph, a_ph, action_space)
+        pi_targ, q_targ, q_pi_targ = actor_critic(x2_ph, a_ph, action_space, **ac_kwargs)
 
     # Use "done" variable to cancel future value when at end of episode
     # The stop_gradient means inputs to the operation will not factor into gradients
@@ -39,6 +41,29 @@ def ddpg(env, discount, batch_size, polyak):
                if ('actor-critic' in v.name and 'dense' in v.name)]
     targ_vars = [v for v in tf.trainable_variables()
                if ('target' in v.name and 'dense' in v.name)]
+
+    # TEST
+    # TODO comment out once verified
+    # ex_targ_var = targ_vars[0][:3, :3]
+    # ex_ac_var = ac_vars[0][:3, :3]
+    # sess = tf.Session()
+    # sess.run(tf.global_variables_initializer())
+    # ex_targ_var_0 = sess.run(ex_targ_var)
+    # ex_ac_var_0 = sess.run(ex_ac_var)
+    # print(ex_targ_var_0)
+    # print(ex_ac_var_0)
+
     for i in range(len(targ_vars)):
-        tf.assign(targ_vars[i], polyak * ac_vars[i] + (1 - polyak) * targ_vars[i])
+        targ_vars[i] = targ_vars[i].assign(polyak * ac_vars[i] + (1 - polyak) * targ_vars[i])
+    
+    # TEST
+    # TODO comment out once verified
+    # ex_targ_var = targ_vars[0][:3, :3]
+    # ex_ac_var = ac_vars[0][:3, :3]
+    # ex_targ_var_1 = sess.run(ex_targ_var)
+    # ex_targ_var_1_np = polyak * ex_ac_var_0 + (1 - polyak) * ex_targ_var_0
+    # print(ex_targ_var_1)
+    # print(ex_targ_var_1_np)
+    # assert np.allclose(ex_targ_var_1, ex_targ_var_1_np)
+
     
